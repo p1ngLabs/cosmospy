bd3526dad3adce141d9b027be46b53c2c1a2b019
phuc1nguyen <phuc1nguyen@proton.me>
2024-05-12 19:15:32 +0700

    Problem 6 of the third project

--- project2/problem6.py
+++ project2/problem6.py
@@ -6,12 +6,29 @@ def get_min_max(ints):
   Args:
       ints(list): list of integers containing one or more integers
   """
-  pass
+  if not ints or len(ints) == 0:
+    return None, None
+    
+  min = max = ints[0]
+    
+  for num in ints:
+    if type(num) is not int:
+      return None, None
 
-### Example Test Case of Ten Integers
-import random
+    if num < min: min = num
+    elif num > max: max = num
+    
+  return min, max
 
-l = [i for i in range(0, 10)]  # a list containing 0 - 9
-random.shuffle(l)
 
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+# Test Case 1: Sanity test
+print ("Pass" if ((1, 9) == get_min_max([2, 7, 3, 6, 4, 1, 9, 8, 5])) else "Fail")
+
+# Test Case 2: Empty list
+print("Pass" if ((None, None) == get_min_max([])) else "Fail")
+
+# Test Case 3: None as input
+print("Pass" if ((None, None) == get_min_max(None)) else "Fail")
+
+# Test Case 4: List element is not integer
+print("Pass" if ((None, None) == get_min_max([12, "test", 34])) else "Fail")
